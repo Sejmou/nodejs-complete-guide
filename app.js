40d92c5d5bcafb44c12e70641820cfca3fc87a1a
Sejmou <samo.kolter@gmail.com>
2022-08-30 16:31:23 +0200

    basic request body parsing

--- app.js
+++ app.js
@@ -17,10 +17,27 @@ const server = http.createServer((req, res) => {
   }
   // upon sending form content, browser will navigate to URL mentioned under action (in this case /message)
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync(
-      'message.txt',
-      'Received something from a client! TODO: parse actual request body'
-    );
+    const body = [];
+    // data event fires whenever new data chunk is available; listener receives chunk
+    req.on('data', chunk => {
+      console.log('received chunk', chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      // when this fires, we received all the data chunks!
+      // now we can concatenate the current (complete) content of body to the globally available buffer
+      // there's also a utility function for parsing the buffer content as a string
+      // note that in this case we just assume the data will be a string
+      // the correct approach always depends on the type of data we receive
+      const parsedBody = Buffer.concat(body).toString();
+      console.log('received all chunks, parsed result:', parsedBody);
+      // now, we're ready to write the message to a local file
+      fs.writeFileSync(
+        'message.txt',
+        parsedBody.split('=')[1] // we expect message to be string "message=[messageContent]" where [mesageContent] is the actual form input content
+      );
+      // side note: we would actually need to parse special characters like exclamation marks as they are URL-encoded by default
+    });
     // 302 === redirect status code
     res.statusCode = 302;
     // Location header specifies where client browser should be redirected to
